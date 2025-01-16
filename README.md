31a5cedeba8c202994f816af2b17335d5a9d40f7
caioBortolucciwba <caio.bortolucci@wba.com.br>
2025-01-16 17:00:39 -0300

    Subindo cypress

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Automacao_2_0
 #Criando projeto no cypress
+
+####Instalação cypress
+npm init -y 
+npm install cypress--save-dev
+npm install mochawesome mochawesome-merge mochawesome-report-generator --save-dev
+
+####Plugin de upload
+npm install --save-dev cypress-file-upload
+dentro de commands adicionar abaixo:
+import 'cypress-file-upload';
+
+
+####Comandos cypress####
+Abrir cypress
+npm cypress open
+
---
+++ cypress.config.js
@@ -0,0 +1,41 @@
+const { defineConfig } = require("cypress");
+const cucumber = require("cypress-cucumber-preprocessor").default;
+const mochawesomeMerge = require("mochawesome-merge");
+const mochawesomeReportGenerator = require("mochawesome-report-generator");
+
+module.exports = defineConfig({
+  e2e: {
+    verifyBaseUrl: false,
+    defaultCommandTimeout: 50000,
+    viewportWidth: 1380,
+    viewportHeight: 720,
+    baseUrl: "https://d-new.wba.com.br/login/82240abc-fd82-4c4b-8266-8deebbad9979",
+    specPattern: "cypress/e2e/features",
+    setupNodeEvents(on, config) {
+      on("file:preprocessor", cucumber());
+      // Processo para gerar o relatório único após a execução
+      on("after:run", async () => {
+        const reportJson = await mochawesomeMerge({
+          files: ["cypress/reports/*.json"]
+        });
+
+        mochawesomeReportGenerator.create(reportJson, {
+          reportDir: "cypress/reports",
+          overwrite: true,
+          html: true,
+          json: true
+        });
+      });
+
+      
+    },
+  },
+
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    overwrite: false,
+    html: true,
+    json: true,
+  },
+});
