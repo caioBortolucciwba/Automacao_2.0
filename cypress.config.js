f891394c3d205d6f1281ad143d886528aff949cd
danielwba <daniel.souza@wba.com.br>
2025-02-21 15:15:17 -0300

    feito cria cpf e tira pontos e traço cpf para login e senha

--- cypress.config.js
+++ cypress.config.js
@@ -14,6 +14,7 @@ module.exports = defineConfig({
     viewportWidth: 1380,
     viewportHeight: 720,
     baseUrl: "https://dnew.wba.com.br/login/82240abc-fd82-4c4b-8266-8deebbad9979",
+    baseUrl2:"https://tportal.wba.com.br:8445/realms/portal-demo/protocol/openid-connect/auth?client_id=portal-angular&redirect_uri=https%3A%2F%2Fdportal.wba.com.br%2F&state=c105d7a2-f248-498f-8aba-645ea981b56b&response_mode=fragment&response_type=code&scope=openid&nonce=95cf3e7c-d497-4c14-8d64-a5e02c9eaf1d",
     specPattern: "cypress/e2e/features/**",
     setupNodeEvents(on, config) {
       on("file:preprocessor", cucumber());
--- cypress/e2e/pages/Painel_Cliente/PainelCliente_Karina_Cedente.js
+++ cypress/e2e/pages/Painel_Cliente/PainelCliente_Karina_Cedente.js
@@ -1,13 +1,15 @@
-
-class PainelCliente{
-    painelClienteKarinaSacado(){
+import { gerarCPF, gerarCNPJ, gerarRG } from '../../../support/utils';
+class PainelCliente {
+    painelClienteKarinaSacado() {
+        const cpf = gerarCPF();
+        const cpfsemformatacao = cpf.replace(/\D/g, '');
         cy.get('.link-configuracoes > .icon').click();
         cy.get('#card-menu-0 > .card-titulo-texto > .header').click();
         cy.get('#card-menu-9 > .card-icon').click();
         cy.get('#PERFIL_ACESSO_SACADO').click();
         cy.get('#btn-novo > .ng-star-inserted').click();
         cy.get("#mat-input-2").type('Teste Karina Sacado');
-        cy.get('#btn-salvar > .ng-star-inserted').click();      
+        cy.get('#btn-salvar > .ng-star-inserted').click();
         cy.get('#menu-lateral-CADASTRO > .flex-column > .menu-click-js').click();
         cy.get('#item-menu-1 > span').click();
         cy.get("#bt-search").click();
@@ -19,48 +21,58 @@ class PainelCliente{
         cy.get('#item-menu-12').click();
         cy.get('#btn-add-contato > .ng-star-inserted').click();
         cy.get("#mat-input-33").click();
-        cy.get("#mat-input-33").type('653.439.135-04');
+
+        cy.get('#mat-input-33').type(cpf);
+        cy.writeFile('cypress/fixtures/cpf.json', { cpf: cpfsemformatacao });
+
+
+
+
+
         cy.get('#mat-input-34').click();
         cy.get('#mat-input-34').type('Teste Karina Sacado Painel')
         cy.get('#mat-input-35').click();
-        cy.get('#mat-input-35').type('daniel.souza@wba.com');
+        cy.get('#mat-input-35').type('daniel.souza@wba.com.br');
         cy.get('#btn-add-empresa').click();
         cy.get('#select-empresa-sacado0 > .mat-select-trigger > .mat-select-value').click();
+        cy.contains('PROPRIA - Karina FAC').click();
         cy.get('#select-perdil-sacado0 > .mat-select-trigger > .mat-select-value').click();
-        cy.get('#mat-option-80 > .mat-option-text').click();
+        cy.get('#mat-option-85 > .mat-option-text').click();
         cy.get('.ml30 > .btn > .ng-star-inserted').click();
         cy.contains('Usuário criado com sucesso!').should('be.visible');
-
-
-
-        // teste
 
         //steps da página principal de cadastro do novo cliente em configurações.
 
     }
 
-    PaginaPainelCLiente(){
-        visit();
+    PaginaPainelCLiente() {
+        cy.visit('https://dnew.wba.com.br'); // Página inicial do primeiro portal
+
+        cy.origin('https://tportal.wba.com.br:8445', () => {
             cy.visit('https://tportal.wba.com.br:8445/realms/portal-demo/protocol/openid-connect/auth?client_id=portal-angular&redirect_uri=https%3A%2F%2Fdportal.wba.com.br%2F&state=c105d7a2-f248-498f-8aba-645ea981b56b&response_mode=fragment&response_type=code&scope=openid&nonce=95cf3e7c-d497-4c14-8d64-a5e02c9eaf1d');
-    
-            fillUsername(username) 
-                cy.get('#mat-input-0').type('CPF'); 
-              
-            
-              fillPassword(password) 
-                cy.get('#mat-input-1').type('CPF'); 
-              
-            
-              submit() 
-                cy.get('.btn > .ng-star-inserted > span').click(); 
-    }
-    
 
-    CadastroConcluido(){ 
+            cy.fixture('cpf.json').then((data) => {
+                cy.get('#inputCpf2').type(data.cpf);
+                cy.get('#password').type(data.cpf);
+                cy.get('.btn > .ng-star-inserted > span').click();
 
-       cy.log('Todos os campos obrigatórios foram preenchidos. Painel do Cliente concluído com sucesso.');
+
+                //cy.get('#login-button').click();
+            });
+
+            cy.url().should('include', '/dashboard'); // Validação pós-login
+        });
     }
+
+
+
+
+    CadastroConcluido() {
+
+        cy.log('Todos os campos obrigatórios foram preenchidos. Painel do Cliente concluído com sucesso.');
+    }
+
 }
 
-  
-  export default  PainelCliente;
+
+export default PainelCliente;
