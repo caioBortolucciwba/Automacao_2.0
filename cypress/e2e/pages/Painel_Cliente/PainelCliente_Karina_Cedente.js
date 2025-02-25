2640bf03083ea4b1c34173536698e85e5574c8a7
Caio Fabricio Faria Bortolucci <caio.bortolucci@wba.br>
2025-02-25 17:29:21 -0300

    Subindo branch caio com ajuste em painel do cliente e cobranca

--- cypress/e2e/pages/Painel_Cliente/PainelCliente_Karina_Cedente.js
+++ cypress/e2e/pages/Painel_Cliente/PainelCliente_Karina_Cedente.js
@@ -18,7 +18,7 @@ class PainelCliente {
         cy.get('#input-search').type('888.999.777-04');
         cy.get("#bt-search").click();
         cy.get('body').type('{esc}');
-
+        ////////
         cy.get("#conteudo-geral > home > div.meuBode.ng-star-inserted > div > pessoa-grid-list > div.full-row.fl.mt15.mb30 > w-table > form > table > tbody > tr:nth-child(1) > td.semBefore.ng-star-inserted > span > span > fa-icon:nth-child(2) > svg").click();
         cy.get('#item-menu-12').click();
         cy.get('#btn-add-contato > .ng-star-inserted').click();
