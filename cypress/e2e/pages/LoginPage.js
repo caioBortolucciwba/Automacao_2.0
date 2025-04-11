6a50d32b3fd016b9c4ff4f97aa9e8442038eff2b
Caio Fabricio Faria Bortolucci <caio.bortolucci@wba.br>
2025-04-11 11:30:24 -0300

    Subida da branch Caio com Ajuste em Login

--- cypress/e2e/pages/LoginPage.js
+++ cypress/e2e/pages/LoginPage.js
@@ -1,14 +1,14 @@
-// let urlBase = "https://dnew";  // ALTERAR PARA BASE URL QUE DESEJA UTILIZAR.
+let urlBase = "https://dnew";  // ALTERAR PARA BASE URL QUE DESEJA UTILIZAR.
 
 class LoginPage {
   visit() {
-<<<<<<< HEAD
-    cy.visit('https://dnew.wba.com.br/login/82240abc-fd82-4c4b-8266-8deebbad9979');
-=======
-    cy.visit(`https://dnew.wba.com.br/login/82240abc-fd82-4c4b-8266-8deebbad9979`);
->>>>>>> ee9ad7bd4c7bf3213c3439692cb0e430cca95e57
+    cy.visit(`${urlBase}.wba.com.br/login/82240abc-fd82-4c4b-8266-8deebbad9979`);
   }
 
+ urlBaseUtilizada(){
+    return (`${urlBase}`);
+ }
+ 
   fillUsername(username) {
     cy.get('#mat-input-0').type(username); 
   }
